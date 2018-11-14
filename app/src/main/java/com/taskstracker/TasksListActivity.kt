55bda3cd40048f63c53a6340cafc2124c2f68c10
LordSkin <bartek.kramnik@wp.pl>
2018-11-14 18:20:26 +0100

    Initial commit

---
+++ app/src/main/java/com/taskstracker/TasksListActivity.kt
@@ -0,0 +1,12 @@
+package com.taskstracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TasksListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks_list)
+    }
+}
